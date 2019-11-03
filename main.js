63a9b68e67fc92e2b7647b42cb7879cf3800e0ca
viniciusald <viniciusaldesouza@gmail.com>
2019-11-03 18:18:56 -0300

    adicionado o main.js e configurado as imagens ao JS

--- main.js
+++ main.js
@@ -20,49 +20,4 @@ imgs.forEach(img => {
 
 cardBoard.innerHTML = cardHTML + cardHTML;
 
-/** Fim da Renderização HTML */
 
-const cards = document.querySelectorAll(".memory-card");
-let firstCard, secondCard;
-let lockCards = false;
-
-function flipCard() {
-  if (lockCards) return false;
-  this.classList.add("flip");
-
-  if (!firstCard) {
-    firstCard = this;
-    return false;
-  }
-
-  secondCard = this;
-
-  checkForMatch();
-}
-
-function checkForMatch() {
-  let isMatch = firstCard.dataset.card === secondCard.dataset.card;
-
-  !isMatch ? unFlipCards() : resetCards(isMatch);
-}
-
-function unFlipCards() {
-  lockCards = true;
-  setTimeout(() => {
-    firstCard.classList.remove("flip");
-    secondCard.classList.remove("flip");
-
-    resetCards();
-  }, 1000);
-}
-
-function resetCards(isMatch = false) {
-  if (isMatch) {
-    firstCard.removeEventListener("click", flipCard);
-    secondCard.removeEventListener("click", flipCard);
-  }
-
-  [firstCard, secondCard, lockCards] = [null, null, false];
-}
-
-cards.forEach(card => card.addEventListener("click", flipCard));
